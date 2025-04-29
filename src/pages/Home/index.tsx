b6cee06128f1762db495fbe00183086b50977e13
Guilherme <guilhermeacacio3@gmail.com>
2025-04-28 22:00:42 -0300

    Adiciona a biblioteca date-fns e implementa lógica de contagem regressiva no componente Home

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,19 +1,19 @@
 import { Play } from "phosphor-react";
 import { zodResolver } from "@hookform/resolvers/zod";
-import * as zod from 'zod'
+import * as zod from "zod";
 import { useForm } from "react-hook-form";
-import { useState } from "react";
+import { useEffect, useState } from "react";
+import { differenceInSeconds } from "date-fns";
 
 import {
-  CountdownContainer, 
-  FormContainer, 
-  HomeContainer, 
-  MinutesAmountInput, 
-  Separator, 
-  StartCountdownButton, 
-  TaskInput } from "./styles";
-
-
+  CountdownContainer,
+  FormContainer,
+  HomeContainer,
+  MinutesAmountInput,
+  Separator,
+  StartCountdownButton,
+  TaskInput,
+} from "./styles";
 
 const newCycleFormValidationSchema = zod.object({
   task: zod.string().min(1, "Informe a tarefa"),
@@ -23,30 +23,44 @@ const newCycleFormValidationSchema = zod.object({
     .max(60, "O ciclo precisa ser de no maximo 60 minutos "),
 });
 
-
-
-type NewCycleFormData = zod.infer<typeof newCycleFormValidationSchema>
+type NewCycleFormData = zod.infer<typeof newCycleFormValidationSchema>;
 
 interface Cycle {
-  id: string
-  task: string
-  minutesAmount: number
+  id: string;
+  task: string;
+  minutesAmount: number;
+  startDate: Date;
 }
 
 export function Home() {
-  const [cycles, setCycles] = useState<Cycle[]>([])
-  const [activeCycleId, setActiveCycleId] = useState<string | null>(null)
-  const [amountsSecondsPassed, setAmountSecondsPassed] = useState(0)
-
+  const [cycles, setCycles] = useState<Cycle[]>([]);
+  const [activeCycleId, setActiveCycleId] = useState<string | null>(null);
+  const [amountsSecondsPassed, setAmountSecondsPassed] = useState(0);
 
   const { register, handleSubmit, watch, reset } = useForm<NewCycleFormData>({
     resolver: zodResolver(newCycleFormValidationSchema),
     defaultValues: {
-      task: '',
+      task: "",
       minutesAmount: 0,
     },
   });
 
+  const activeCycle = cycles.find((cycle) => cycle.id === activeCycleId);
+
+  useEffect(() => {
+    let interval: number
+
+    if (activeCycle) {
+      interval = setInterval(() => {
+        setAmountSecondsPassed(
+          differenceInSeconds(new Date(), activeCycle.startDate)
+        );
+      }, 1000);
+    }
+    return () => {
+      clearInterval(interval);
+    }
+  }, [activeCycle]);
 
   function handleCreateNewCycle(data: NewCycleFormData) {
     const id = String(new Date().getTime());
@@ -55,28 +69,37 @@ export function Home() {
       id,
       task: data.task,
       minutesAmount: data.minutesAmount,
+      startDate: new Date(),
     };
 
-    setCycles((state) => [...state, newCycle])
-    setActiveCycleId(id)
-    reset()
+    setCycles((state) => [...state, newCycle]);
+    setActiveCycleId(id);
+    setAmountSecondsPassed(0);
+
+    reset();
   }
 
-const activeCycle = cycles.find(cycle => cycle.id === activeCycleId)
+  const totalSeconds = activeCycle ? activeCycle.minutesAmount * 60 : 0;
+  const currentSeconds = activeCycle ? totalSeconds - amountsSecondsPassed : 0;
 
-const totalSeconds = activeCycle ? activeCycle.minutesAmount * 60 : 0 
-const currentSeconds = activeCycle ? totalSeconds - amountsSecondsPassed : 0
+  const minutesAmount = Math.floor(currentSeconds / 60);
+  const secondsAmount = currentSeconds % 60;
 
-const minutesAmount = Math.floor(currentSeconds / 60)
-const secondsAmount = currentSeconds % 60
-
-const minutes = String(minutesAmount).padStart(2, '0')
-const seconds = String(secondsAmount).padStart(2, "0");
+  const minutes = String(minutesAmount).padStart(2, "0");
+  const seconds = String(secondsAmount).padStart(2, "0");
 
 
-const task = watch('task')
-const isSubmitDisabled = !task;
-  
+  useEffect(() => {
+    if (activeCycle) {
+    document.title = `${minutes}:${seconds}`;
+
+    }
+  }, [minutes, seconds, activeCycle])
+
+
+  const task = watch("task");
+  const isSubmitDisabled = !task;
+
   return (
     <HomeContainer>
       <form onSubmit={handleSubmit(handleCreateNewCycle)} action="">
@@ -117,11 +140,14 @@ const isSubmitDisabled = !task;
           <span>{seconds[0]}</span>
           <span>{seconds[1]}</span>
         </CountdownContainer>
-
-        <StartCountdownButton disabled={isSubmitDisabled} type="submit">
-          <Play size={24} />
-          Começar
-        </StartCountdownButton>
+        {activeCycle ? (
+          <div>PARAR</div>
+        ) : (
+          <StartCountdownButton disabled={isSubmitDisabled} type="submit">
+            <Play size={24} />
+            Começar
+          </StartCountdownButton>
+        )}
       </form>
     </HomeContainer>
   );
