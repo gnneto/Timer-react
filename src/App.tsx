f3c5d3d1949d6ec1cf75788f18c0201ed2a3a7fb
Guilherme <guilhermeacacio3@gmail.com>
2025-05-01 23:13:20 -0300

    Adiciona contexto de ciclos e implementa lógica de contagem no componente Home; refatora componentes de formulário e contagem

--- src/App.tsx
+++ src/App.tsx
@@ -4,14 +4,20 @@ import { BrowserRouter } from 'react-router-dom';
 
 import { defaultTheme } from './styles/themes/default';
 import { GlobalStyle } from './styles/global';
+import { CyclesContexProvider } from './contexts/CyclesContext';
+// import { Home } from './pages/Home';
+
 
 export function App() {
+
   return (
     <ThemeProvider theme={defaultTheme}>
       <BrowserRouter>
-        <Router/>
+        <CyclesContexProvider>
+          <Router />
+        </CyclesContexProvider>
       </BrowserRouter>
       <GlobalStyle />
     </ThemeProvider>
-  )
+  );
 }
